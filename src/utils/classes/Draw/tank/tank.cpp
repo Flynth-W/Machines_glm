2b806204edb3206e9ccb64649c64881d5942db4e
flynth <flynth.dev@gmail.com>
2023-06-14 17:27:41 -0400

    Include head and gun

--- src/utils/classes/Draw/tank/tank.cpp
+++ src/utils/classes/Draw/tank/tank.cpp
@@ -4,7 +4,12 @@ void DrawTank::Init(glm::vec3 *position){
     this->body.setPosition(position);
     this->body.setCamera(this->camera);
     this->body.setVetices();
-
+    
+    this->head.setCamera(this->camera);
+    this->head.setVetices();
+    this->head.setModel(&this->body.model);
+    this->head.setPosition(glm::vec3(0.0,0.23,0));
+    this->head.Init();
     this->undercarriageLeft.setCamera(this->camera);
     this->undercarriageLeft.setPosition(glm::vec3(-0.37,0.0,0.0));
     this->undercarriageLeft.setModel(&this->body.model);
@@ -14,16 +19,24 @@ void DrawTank::Init(glm::vec3 *position){
     this->undercarriageRigth.setPosition(glm::vec3(0.37,0.0,0.0));
     this->undercarriageRigth.setModel(&this->body.model);
     this->undercarriageRigth.Init();
+    
+    bullets.setDeltaTime(this->deltaTime);
+    bullets.setCamera(this->camera);
+    bullets.Init();
 }
 void DrawTank::Update(){
     this->body.Update();
+    this->head.Update();
     this->undercarriageLeft.Update();
     this->undercarriageRigth.Update();
+    bullets.Update();
 }
 void DrawTank::Render(){
     this->body.Render();
+    this->head.Render();
     this->undercarriageLeft.Render();
     this->undercarriageRigth.Render();
+    bullets.Render();
 }
 
 void DrawTank::setCamera(Camera *camera){
@@ -50,5 +63,17 @@ void DrawTank::rotate(float angle){
     }
 }
 void DrawTank::headRotate(double angle){
+    this->head.rotate(angle);
 }
+void DrawTank::gunRotate(double angle){
+    this->head.gun.rotate(angle);
+}
+void DrawTank::shootGun(){
+    //shot
+    //this->head.gun.model;
+    this->bullets.AddBullet(*this->head.gun.model);
+}
+void DrawTank::setDeltaTime(double *deltaTime){
+    this->deltaTime=deltaTime;
+};
 
--- src/utils/classes/Draw/tank/tank.hpp
+++ src/utils/classes/Draw/tank/tank.hpp
@@ -3,23 +3,29 @@
 #include "../../camera/camera.hpp"
 #include "./body/body.hpp"
 #include "./undercarriage/undercarriage.hpp"
-
-
+#include "./head/head.hpp"
+#include "../../sysBullets/sysBullet.hpp"
 class DrawTank{   
     private:
         Camera *camera;
         DrawBodyOfTank body;
         DrawUndercarriage undercarriageLeft;
         DrawUndercarriage undercarriageRigth;
+        DrawHeadOfTurtle head;
+        SysBullet bullets;
+        double *deltaTime;
     public:
         void Init(glm::vec3 *position);
         void Update();
         void Render();
         
         void setCamera(Camera *camera);
+        void setDeltaTime(double *deltaTime);
         glm::mat4 getModel();
 
         void move(float translate);
         void rotate(float angle);
         void headRotate(double angle);
+        void gunRotate(double angle);
+        void shootGun();
 };
